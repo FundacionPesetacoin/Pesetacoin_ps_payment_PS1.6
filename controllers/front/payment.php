e077fae750a078dd13f2754833a08cd59fe4ea00
marcostrfn <marcos.trfn@gmail.com>
2018-01-18 23:27:46 +0100

    Add files via upload

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -1,28 +1,4 @@
 <?php
-/*
-* 2007-2016 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2016 PrestaShop SA
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
 
 /**
  * @since 1.5.0
@@ -43,23 +19,18 @@ class Pesetacoin_ps_paymentPaymentModuleFrontController extends ModuleFrontContr
 		if (!$this->module->checkCurrency($cart))
 			Tools::redirect('index.php?controller=order');
 
-
-        	
 		$obj_pesetacoin = new PesetaCoinPaymentFunciones();
 		$getPriceEur = $obj_pesetacoin->getPriceEur();
 		$getPriceUsd = $obj_pesetacoin->getPriceUsd();
 		$getPriceBtc = $obj_pesetacoin->getPriceBtc();
 		
-		
+		// tratar error
 		if ($getPriceEur=='--') {
-			// tratar error
-			
+			die($this->module->l('El método de pago no está disponible. ¿Problemas de red?', mod='pesetacoin_ps_payment'));
 		}
 		
-                $importe = $cart->getOrderTotal(true, Cart::BOTH);
+        $importe = $cart->getOrderTotal(true, Cart::BOTH);
 		$importePtc = $importe / $getPriceEur;
-
-		// $direccion = Configuration::get('PTC_PAYMENT_DIR');
 
 		$sql = "SELECT COUNT(token_ptc) FROM PREFIX_pesetacoin_ps_payment WHERE estado_ptc = 0 AND id_pedido_ptc = '0'";
 		$mysql = $this->prepareSql($sql);
@@ -70,18 +41,14 @@ class Pesetacoin_ps_paymentPaymentModuleFrontController extends ModuleFrontContr
 		$direccion_pago = Db::getInstance()->getValue($mysql);
 		Configuration::updateValue('PTC_PAYMENT_DIR_PAGO', $direccion_pago); 
 			
-			
 		Configuration::updateValue('PTC_PAYMENT_IMPORTE_PTC', $importePtc);
-                Configuration::updateValue('PTC_PAYMENT_IMPORTE', $importe);
+        Configuration::updateValue('PTC_PAYMENT_IMPORTE', $importe);
 
-		
         $this->context->smarty->assign(array(
 			'nbProducts' => $cart->nbProducts(),
-
-                        'numero_direciones' => $numero_direciones,
+            'numero_direciones' => $numero_direciones,
 			'importePtc' => $importePtc,
 			'direccion' => $direccion_pago, // $direccion,
-			
 			'cust_currency' => $cart->id_currency,
 			'currencies' => $this->module->getCurrency((int)$cart->id_currency),
 			'total' => $cart->getOrderTotal(true, Cart::BOTH),
@@ -90,10 +57,6 @@ class Pesetacoin_ps_paymentPaymentModuleFrontController extends ModuleFrontContr
 			'this_path_pesetacoin_ps_payment' => $this->module->getPathUri(),
 			'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->module->name.'/'
 		));
-		
-		
-		
-		
 
 		$this->setTemplate('payment_execution.tpl');
 	}
@@ -103,11 +66,8 @@ class Pesetacoin_ps_paymentPaymentModuleFrontController extends ModuleFrontContr
 	{
 	  // Get install SQL file content
 	  $sql_content = $sql;
-
 	  // Replace prefix and store SQL command in array
 	  $sql_content = str_replace('PREFIX_', _DB_PREFIX_, $sql_content);
-	  
-	  
 	  return $sql_content;
 	}
 	
--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -1,28 +1,4 @@
 <?php
-/*
-* 2007-2016 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2016 PrestaShop SA
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
 
 /**
  * @since 1.5.0
@@ -46,7 +22,7 @@ class Pesetacoin_ps_paymentValidationModuleFrontController extends ModuleFrontCo
 			}
 
 		if (!$authorized)
-			die($this->module->l('This payment method is not available.', 'validation'));
+			die($this->module->l('El método de pago no está disponible.', mod='pesetacoin_ps_payment'));
 
 		$customer = new Customer($cart->id_customer);
 
@@ -56,16 +32,8 @@ class Pesetacoin_ps_paymentValidationModuleFrontController extends ModuleFrontCo
 		$currency = $this->context->currency;
 		$total = (float)$cart->getOrderTotal(true, Cart::BOTH);
 
-/*
-		$mailVars =	array(
-			'{pesetacoin_ps_payment_name}' => Configuration::get('PESETACOIN_PS_PAYMENT_NAME'),
-			'{pesetacoin_ps_payment_address}' => Configuration::get('PESETACOIN_PS_PAYMENT_ADDRESS'),
-			'{pesetacoin_ps_payment_address_html}' => str_replace("\n", '<br />', Configuration::get('PESETACOIN_PS_PAYMENT_ADDRESS')));
-*/
 
-
-
-global $currency;
+		global $currency;
 		$my_currency_iso_code = $currency->iso_code;
 		$my_currency_sign = $currency->sign;
 
